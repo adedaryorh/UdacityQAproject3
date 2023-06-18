81324a54257f4782c06d4e1f2377f7dfeb4be06f
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-06-18 08:28:36 +0100

    Update input.tf

--- terraform/modules/resource_group/input.tf
+++ terraform/modules/resource_group/input.tf
@@ -1,3 +1,8 @@
 # Resource Group
-variable location {}
-variable "resource_group" {}
+# Resource Group
+variable "location" {
+  default = "eastus"
+}
+variable "resource_group" {
+  default = "UDACITY-rg"
+}
