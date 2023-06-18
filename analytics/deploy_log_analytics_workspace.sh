c64bea76c7cb11fd433c01ee388f3d57eb663661
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-06-18 08:43:04 +0100

    Delete analytics directory

--- analytics/deploy_log_analytics_workspace.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-RESOURCE_GROUP_NAME=ensuring-qa-release-rg
-
-az deployment group create --resource-group "$RESOURCE_GROUP_NAME" --name deploy-log --template-file log_analytics_template_file.json
